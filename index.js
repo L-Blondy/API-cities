0abaa53da89680cc2268b03e5a39b16a46e82abf
Laurent Blondy <laurent.blondy1@gmail.com>
2020-04-03 16:36:29 +0200

    Add cool face API

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const express = require('express');
 const cors = require('cors');
 var helmet = require('helmet');
 const Search = require('./src/Search.js');
-const fs = require('fs').promises;
+// const http = require('http');
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-console.log('environement=', process.env.NODE_ENV);
+console.log('environment=', process.env.NODE_ENV);
 
 app.use(cors());
 app.use(helmet());
@@ -15,14 +16,17 @@ app.use(helmet());
 app.get('/', (req, res) => {
 	const { keyword, maxResults, sortBy } = req.query;
 
-	console.time('a');
 	const newSearch = new Search(keyword, {
 		sortBy,
 		maxResults
 	});
-	console.timeEnd('a');
 
 	res.send(newSearch.matches);
 });
 
-app.listen(3000, '127.0.0.1');
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+// setInterval(() => {
+// 	http.get(`http://${ process.env.PROJECT_DOMAIN }.glitch.me/`);
+// }, 280000);
